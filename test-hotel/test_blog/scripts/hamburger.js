eb51509c277fca6bfd53cce3f72eae96ede5f69e
IrenNov <irennov524@gmail.com>
2024-12-02 22:21:03 +0300

    second

--- test-hotel/test_blog/scripts/hamburger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.header__nav');
-  
-    hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('is-active');
-      nav.classList.toggle('overflow');
-    });
-  });
